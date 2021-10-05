f97c647149754138011698a713da3f0bf2ec5830
Malins062 <6328673@gmail.com>
2021-10-05 19:31:40 +0300

    Editing menu functional

--- main.py
+++ main.py
@@ -2,18 +2,18 @@
 import os
 
 # Название программы, выводимое на экран
-TITLE_PROGRAM = '--- ФОТОКОПИРОВАНИЕ НА СЕРВИС "ЯНДЕКС ДИСК" ---'
+TITLE_PROGRAM = '--- ФОТОКОПИРОВАНИЕ НА СЕРВИС ОБЛАЧНЫЙ СЕРВИС ---'
 
 
 # Список допустимых команд программы, их описание и определение запускаемых функций
-commands = [{'1': ('ВКонтакте;', 'https://vk.com/dev/photos.get'),
-             '2': ('Однокласники;', 'https://apiok.ru/'),
-             '3': ('Инстаграмм', 'https://www.instagram.com/developer/'),
+commands = [{'1': ('Яндекс диск;', ''),
+             '2': ('Google drive;', 'command_s'),
              '0': ('выход из программы.\n', exit)
              },
-            {'1': ('Яндекс диск;', ''),
-             '2': ('Google drive;', 'command_s'),
-             '3': ('возврат в предыдущее меню;', 'command_q'),
+            {'1': ('ВКонтакте;', 'https://vk.com/dev/photos.get'),
+             '2': ('Однокласники;', 'https://apiok.ru/'),
+             '3': ('Инстаграмм', 'https://www.instagram.com/developer/'),
+             '9': ('возврат в предыдущее меню;', 'command_q'),
              '0': ('выход из программы.\n', exit)
              }
             ]
@@ -34,20 +34,29 @@ def main(cmd):
         print(f'\n*** ОШИБКА! Выполнение команды "{command}" не допустимо! ***\n')
         input('Для продолжения работы нажмите клавишу "Enter"...')
 
-    while True:
+    is_menu_1 = True
+    while is_menu_1:
         os.system('cls||clear')
         print(f'{TITLE_PROGRAM}\n')
 
-        print('Команды пользователя, для выбора источника копирования фотографий:')
-        for key_command, name_command in cmd.items():
+        print('Выберите ресурс назначения копирования фотографий:')
+        for key_command, name_command in cmd[0].items():
             print(f'\t{key_command} – {name_command[0]}')
 
         print('Введите команду:', end=' ')
         command = str(input().strip()).lower()
-        return cmd.get(command, [None, invalid_command])[1]
+        cmd.get(command, [None, invalid_command])[1]()
+
+        is_menu_2 = True
+        while not is_menu_2:
+            print('Выберите источник копирования фотографий:')
+            for key_command, name_command in cmd[1].items():
+                print(f'\t{key_command} – {name_command[0]}')
+
+            print('Введите команду:', end=' ')
+            command = str(input().strip()).lower()
+            cmd.get(command, [None, invalid_command])[1]()
 
 
 if __name__ == '__main__':
-
-    what_to_do = main(commands[0])
-    print(what_to_do)
+    main(commands)
