da774ceb78864e2a8adf43abddeadbb84dc8c46d
Malins062 <6328673@gmail.com>
2021-10-06 12:24:14 +0300

    Completed menu for program

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ TITLE_PROGRAM = '--- ФОТОКОПИРОВАНИЕ НА СЕРВИС ОБЛАЧ
 
 
 # Список допустимых команд программы, их описание и определение запускаемых функций
-commands = [{'1': ('Яндекс диск;', ''),
-             '2': ('Google drive;', 'command_s'),
-             '0': ('выход из программы.\n', exit)
+commands = [{'1': ('Яндекс диск;', 1, 'https://yandex.ru/dev/disk/poligon/'),
+             '2': ('Google drive;', 2, 'GoogleDrive API'),
+             '0': ('выход из программы.\n', 0)
              },
-            {'1': ('ВКонтакте;', 'https://vk.com/dev/photos.get'),
-             '2': ('Однокласники;', 'https://apiok.ru/'),
-             '3': ('Инстаграмм', 'https://www.instagram.com/developer/'),
-             '9': ('возврат в предыдущее меню;', 'command_q'),
-             '0': ('выход из программы.\n', exit)
+            {'1': ('ВКонтакте;', 1, 'https://vk.com/dev/photos.get'),
+             '2': ('Однокласники;', 2, 'https://apiok.ru/'),
+             '3': ('Инстаграмм', 3, 'https://www.instagram.com/developer/'),
+             '9': ('возврат в предыдущее меню;', 9, 'Up'),
+             '0': ('выход из программы.\n', 0)
              }
             ]
 
@@ -26,36 +26,75 @@ def main(cmd):
     :return:
     """
 
-    def invalid_command():
+    # Признак не существующей команды меню
+    error_command = 'Invalid'
+
+    def init_screen():
+        """
+        Функция очистки экрана пользователя и вывода заставки
+        """
+        os.system('cls||clear')
+        print(f'{TITLE_PROGRAM}\n')
+
+    def invalid_command(err_cmd):
         """
         Вывод ошибки на экран, при вводе неверной команды
         :return:
         """
-        print(f'\n*** ОШИБКА! Выполнение команды "{command}" не допустимо! ***\n')
+        print(f'\n*** ОШИБКА! Выполнение команды "{err_cmd}" не допустимо! ***\n')
         input('Для продолжения работы нажмите клавишу "Enter"...')
 
-    is_menu_1 = True
-    while is_menu_1:
-        os.system('cls||clear')
-        print(f'{TITLE_PROGRAM}\n')
-
+    # Параметры текущей, выбранной команды:
+    #       destination - ресурс назначения комирования фотографий
+    #       resource - ресурс откуда необходимо копиравть фотографии
+    status_command = {'destination': '',
+                      'resource': ''
+                      }
+    is_exit = False
+    while not is_exit:
+        init_screen()
         print('Выберите ресурс назначения копирования фотографий:')
         for key_command, name_command in cmd[0].items():
             print(f'\t{key_command} – {name_command[0]}')
 
         print('Введите команду:', end=' ')
         command = str(input().strip()).lower()
-        cmd.get(command, [None, invalid_command])[1]()
+        status_command['destination'] = cmd[0].get(command, error_command)
 
-        is_menu_2 = True
-        while not is_menu_2:
-            print('Выберите источник копирования фотографий:')
-            for key_command, name_command in cmd[1].items():
-                print(f'\t{key_command} – {name_command[0]}')
+        # Проверка команды подменю - выбран ли пункт ВЫХОД
+        if status_command['destination'][1] == 0:
+            is_exit = True
+        # Проверка команды подменю - СУЩЕСТВУЕТ ЛИ ВЫБРАННАЯ КОМАНДА
+        elif status_command['destination'] == error_command:
+            invalid_command(command)
+        else:
+            is_menu_out = False
+            while not is_menu_out:
+                init_screen()
+                print('Выберите источник копирования фотографий:')
+                for key_command, name_command in cmd[1].items():
+                    print(f'\t{key_command} – {name_command[0]}')
 
-            print('Введите команду:', end=' ')
-            command = str(input().strip()).lower()
-            cmd.get(command, [None, invalid_command])[1]()
+                print('Введите команду:', end=' ')
+                command = str(input().strip()).lower()
+                status_command['resource'] = cmd[1].get(command, error_command)
+
+                # Проверка команды подменю - выбран ли пункт ВЫХОД
+                if status_command['resource'][1] == 0:
+                    is_exit = True
+                    is_menu_out = True
+                # Проверка команды подменю - СУЩЕСТВУЕТ ЛИ ВЫБРАННАЯ КОМАНДА
+                elif status_command['resource'] == error_command:
+                    invalid_command(command)
+                # Проверка команды подменю - выбран ли пункт ВОЗВРАТ В ГЛАВНОЕ МЕНЮ
+                elif status_command['resource'][1] == 9:
+                    is_menu_out = True
+                else:
+                    init_screen()
+                    print(f'Источник импорта фотографий: {status_command["resource"][2]}.')
+                    print(f'Хранилище фотографий: {status_command["destination"][2]}.')
+                    exit(0)
+    return "До встречи!"
 
 
 if __name__ == '__main__':
